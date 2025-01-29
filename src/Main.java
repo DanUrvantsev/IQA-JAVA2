a194c2f76c9bc3cf8c2c1e35fbdfd665544bb5d0
Danila <laorant8@gmail.com>
2025-01-29 23:42:33 +0700

    Inition commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Стоимость билета
+        int price = 1679;
+        // Расчет бонусов
+        int bonus = price/20;
+        System.out.println("Бонус:"+bonus);
+    }
+}
